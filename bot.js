7032b35f8b1a3edc0be8d6c987ba4aee239aa8d7
Boootat888 <44114402+Boootat888@users.noreply.github.com>
2018-10-13 20:58:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -243,12 +243,6 @@ if (message.content.startsWith(prefix + 'mypoints')) {
 
 
 
-
-            }
-    
-    });
-
-
 client.on("message", function(message) {
    if(message.content.startsWith(prefix + "rps")) {
     let messageArgs = message.content.split(" ").slice(1).join(" ");
